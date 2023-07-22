7527569ba571944ca4b38839a4601f678c03f90e
Jeanproject <116390051+Jeanproject@users.noreply.github.com>
2023-07-21 20:02:41 -0500

    Taller

---
+++ EjemploBD/src/VIEW/View_Est_Consola.java
@@ -0,0 +1,15 @@
+package Poo;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class View_Est_Consola {
+    public static void main(String[] args) {
+        ConexionDB_Est obj= new ConexionDB_Est();
+        System.out.println(obj.insertarEstudiante(new Estudiante("Mateo","110459937","ikestrella",6)));
+        ArrayList<Estudiante> listaEst=obj.getListaEstudiantes();
+        for(Estudiante est: listaEst){
+            System.out.println(est);
+        }
+    }
+}
